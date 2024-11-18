165d4a4ad589864a94a8ffe09b2b2bbdc018fc1c
r0oman01 <roman.gabriel123@hotmail.com>
2024-11-18 20:18:42 -0300

    Add files via upload

---
+++ Tabuada.py
@@ -0,0 +1,11 @@
+#variáveis
+contador = int()
+
+
+#processamento
+valor_1 = int(input("Digite um valor: "))
+
+#sequência de 0 a 10
+for contador in range(11):
+    print(valor_1, " x ", contador, " = ", valor_1 * contador) 
+
