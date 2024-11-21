19a61af1c0cc644fd1284b197096dc66bd9b7f57
r0oman01 <roman.gabriel123@hotmail.com>
2024-11-21 11:06:41 -0300

    Add files via upload

---
+++ Validacao_e_repeticao.py
@@ -0,0 +1,15 @@
+#variáveis
+
+#processamento
+valor = int(input("Digite um valor: "))
+
+#repetição da condição
+while valor > 11:
+    print("Valor inváliido")
+    input("Digite um valor: ")
+
+else:
+    print("Valor válido!")
+    
+
+
