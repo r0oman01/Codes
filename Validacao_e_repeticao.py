970372fd1c5a21406a14177f255052357a403479
r0oman01 <roman.gabriel123@hotmail.com>
2024-11-22 00:21:50 -0300

    Update Validacao_e_repeticao.py

--- Validacao_e_repeticao.py
+++ Validacao_e_repeticao.py
@@ -1,15 +1,27 @@
 #variáveis
+resto = int()
+media = float()
+x = int()
 
 #processamento
-valor = int(input("Digite um valor: "))
+num_1 = float(input("Digite um valor: "))
+num_2 = float(input("Digite um valor: "))
+
+resto_1 = num_1%2
+resto_2 = num_2%2
+
+media = (num_1 + num_2) / 2
+
 
 #repetição da condição
-while valor > 11:
-    print("Valor inváliido")
-    input("Digite um valor: ")
+while (resto_1 == 1) and (resto_2 == 1):
+    x = int(input("Digite um valor novamente: "))
 
+    resto_x = x%2
+
+    if (resto_x == 0) or (x == 0):
+        print("----------")
+        break
 else:
-    print("Valor válido!")
-    
-
+    print("-----------")
 
