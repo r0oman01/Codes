6951019a3428f9a629312d7d70c874eb44b1379c
r0oman01 <roman.gabriel123@hotmail.com>
2024-12-02 16:53:10 -0300

    Add files via upload

---
+++ verificacao_de_media_e_notas_while.py
@@ -0,0 +1,38 @@
+from datetime import time
+from time import sleep
+
+print("---Sistema de cadastro de pessoas---".upper())
+n1: float = 0
+n2: float = 0
+n3: float = 0
+n4: float = 0
+while True:
+    print("---sejam bem-vindos ao programa de notas---".upper())
+    print("""
+    ------Opções------
+    1 - Cadastrar notas
+    2 - Mostrar média
+    3 - Sair
+
+    """)
+    menu = input("Escolha a opção: ")
+    match menu:
+        case "1":
+            print("---Cadastrar Notas---")
+            n1 = float(input("Sua primeira nota: "))
+            n2 = float(input("Sua segunda nota: "))
+            n3 = float(input("Sua terceira nota: "))
+            n4 = float(input("Sua quarta nota: "))
+            print("---cadastro de notas feito com sucesso---".upper())
+            print("-----------------------------")
+            sleep(2)
+        case "2":
+            media = (n1 + n2 + n3 + n4)/4
+            print("---mostrar médias das notas---".upper())
+            print(f"Sua média é: {media}")
+            sleep(2)
+        case "3":
+            print("Saindo...")
+            sleep(3)
+            break
+print("---Fora do sistema---".upper())
