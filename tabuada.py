5fd94dc6f5507b47968d63f725aaa09f0ad08f74
r0oman01 <roman.gabriel123@hotmail.com>
2024-12-05 15:16:39 -0300

    Update tabuada.py

--- tabuada.py
+++ tabuada.py
@@ -9,3 +9,10 @@ valor_1 = int(input("Digite um valor: "))
 for contador in range(11):
     print(valor_1, " x ", contador, " = ", valor_1 * contador) 
 
+cont = int()
+valor_tabuada = int()
+    for cont in range(11):
+     print(f"{cont} x {valor_tabuada} = {valor_tabuada * cont}")
+         for valor_tabuada in range(11):
+           print(f"{cont} x {valor_tabuada} = {valor_tabuada * cont}")
+           print("-"*20)
