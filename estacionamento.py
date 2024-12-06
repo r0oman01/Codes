2657d53e32c7f673421df40b168cbe70f5949ef9
r0oman01 <roman.gabriel123@hotmail.com>
2024-12-06 14:44:14 -0300

    Update estacionamento.py

--- estacionamento.py
+++ estacionamento.py
@@ -1,86 +1,81 @@
-placa_carro = input("Digite a placa: ")
-hora_entrada = int(input("Digite a hora de entrada (0-23): "))
-minuto_entrada = int(input("Digite os minutos de entrada (0-59): "))
-hora_saida = int(input("Digite a hora de saída (0-23): "))
-minuto_saida = int(input("Digite os minutos de saída (0-59): "))
+armazenar_placas = []
+while True:
+    placa_carro = input("Digite a placa: ")
+    hora_entrada = int(input("Digite a hora de entrada (0-23): "))
+    minuto_entrada = int(input("Digite os minutos de entrada (0-59): "))
+    hora_saida = int(input("Digite a hora de saída (0-23): "))
+    minuto_saida = int(input("Digite os minutos de saída (0-59): "))
 
-minutos_entrada = hora_entrada * 60 + minuto_entrada
-minutos_saida = hora_saida * 60 + minuto_saida
-total_minutos = minutos_saida - minutos_entrada
-
-
-print(f"Total de minutos: {total_minutos} minutos.")
-
-if total_minutos <= 60:
-    total = 15
-    print("Até 60 minutos, valor a pagar é de: R$15,00")
-else:
-
-    total_minutos -= 60
-    total = 15
-    print(f"Valor padrão: R$15,00")
+    minutos_entrada = hora_entrada * 60 + minuto_entrada
+    minutos_saida = hora_saida * 60 + minuto_saida
+    total_minutos = minutos_saida - minutos_entrada
 
 
-    while total_minutos >= 60:
+    print(f"Total de minutos: {total_minutos} minutos.")
+
+    if total_minutos <= 60:
+        total = 15
+        print("Até 60 minutos, valor a pagar é de: R$15,00")
+    else:
+
         total_minutos -= 60
-        total += 5
-        print("Hora adicional: R$5,00")
+        total = 15
+        print(f"Valor padrão: R$15,00")
 
 
-    if total_minutos > 0:
-        total += 5
-        print("Hora adicional: R$5,00")
+        while total_minutos >= 60:
+            total_minutos -= 60
+            total += 5
+            print("Hora adicional: R$5,00")
+
+
+        if total_minutos > 0:
+            total += 5
+            print("Hora adicional: R$5,00")
 
 
     lavagem = input("Vai fazer a lavagem? S ou N: ").upper()
     if lavagem != 'N':
         total += 60
         print("A lavagem adiciona R$60,00")
-    print("-" * 20)
-    print(f"Valor total a pagar: R$ {total:.2f}")
+        print("-" * 20)
+        print(f"Valor total a pagar: R$ {total:.2f}")
 
-while True:
-    print("""
-    Registrar outro carro
-1 - Registrar outro carro
-2 - Sair
-    """)
-    menu = str(input("Escolha a opção: "))
-    match menu:
-        case "1":
-            placa_carro = input("Digite a placa: ")
-            hora_entrada = int(input("Digite a hora de entrada (0-23): "))
-            minuto_entrada = int(input("Digite os minutos de entrada (0-59): "))
-            hora_saida = int(input("Digite a hora de saída (0-23): "))
-            minuto_saida = int(input("Digite os minutos de saída (0-59): "))
+        menu = str(input("Quer cadastrar mais um carro: "))
+        match menu:
+            case "1":
+                placa_carro = input("Digite a placa: ")
+                hora_entrada = int(input("Digite a hora de entrada (0-23): "))
+                minuto_entrada = int(input("Digite os minutos de entrada (0-59): "))
+                hora_saida = int(input("Digite a hora de saída (0-23): "))
+                minuto_saida = int(input("Digite os minutos de saída (0-59): "))
 
-            print(f"Total de minutos: {total_minutos} minutos.")
+                print(f"Total de minutos: {total_minutos} minutos.")
 
-            if total_minutos <= 60:
-                total = 15
-                print("Até 60 minutos, valor a pagar é de: R$15,00")
-            else:
+                if total_minutos <= 60:
+                    total = 15
+                    print("Até 60 minutos, valor a pagar é de: R$15,00")
+                else:
 
-                total_minutos -= 60
-                total = 15
-                print(f"Valor padrão: R$15,00")
-
-                while total_minutos >= 60:
                     total_minutos -= 60
-                    total += 5
-                    print("Hora adicional: R$5,00")
+                    total = 15
+                    print(f"Valor padrão: R$15,00")
 
-                if total_minutos > 0:
-                    total += 5
-                    print("Hora adicional: R$5,00")
+                    while total_minutos >= 60:
+                        total_minutos -= 60
+                        total += 5
+                        print("Hora adicional: R$5,00")
 
-            lavagem = input("Vai fazer a lavagem? S ou N: ").upper()
-            if lavagem != 'N':
-                total += 60
-                print("A lavagem adiciona R$60,00")
-            print("-" * 20)
-            print(f"Valor total a pagar: R$ {total:.2f}")
-            print("-" * 20)
-        case "2":
-            break
+                    if total_minutos > 0:
+                        total += 5
+                        print("Hora adicional: R$5,00")
 
+                lavagem = input("Vai fazer a lavagem? S ou N: ").upper()
+                if lavagem != 'N':
+                    total += 60
+                    print("A lavagem adiciona R$60,00")
+                print("-" * 20)
+                print(f"Valor total a pagar: R$ {total:.2f}")
+                print("-" * 20)
+            case "2":
+                break
