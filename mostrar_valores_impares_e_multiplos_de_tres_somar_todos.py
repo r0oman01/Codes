840aae87c4ef9d765ac05b9272e1f133eb09cd25
r0oman01 <roman.gabriel123@hotmail.com>
2024-12-09 21:29:38 -0300

    Update mostrar_valores_impares_e_multiplos_de_tres_somar_todos.py

--- mostrar_valores_impares_e_multiplos_de_tres_somar_todos.py
+++ mostrar_valores_impares_e_multiplos_de_tres_somar_todos.py
@@ -1,7 +1,10 @@
 cont = int()
 soma = int()
+contador = 0
 for cont in range(1,501):
     if cont % 2 == 1 and cont % 3 == 0:
         print(cont)
-        soma = soma + cont
+        contador += 1
+        soma += + cont
 print(soma)
+print(contador)
