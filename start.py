f319c7ea80ddab65eadc56de0aa0bee386fab94d
r0oman01 <roman.gabriel123@hotmail.com>
2025-02-06 13:49:11 -0300

    Update start.py

--- start.py
+++ start.py
@@ -1,45 +1,50 @@
-from flet import  *
+from flet import *
 from login import Login
 from menu import Menu
 
-def start(page:Page):
+def start(page: Page):
     page.title = "Control"
 
-    #criando o objeto Login()
+    # Criando o objeto Login()
     def changepagemenu(event):
         nome = tela_login.inputLogin.value
         senha = tela_login.inputPassword.value
-        if nome and senha:
-            if nome == "carlos" and senha == "123":
-                page.go("/menu")
-                page.update()
-            else:
-                tela_login.inputPassword.error_text = "Digite a senha"
-                tela_login.inputLogin.error_text = "Digite o login"
 
+        # Verificando se os campos estão preenchidos corretamente
+        if nome == "carlos" and senha == "123":
+            page.go("/menu")
+            page.update()
         else:
-            tela_login.inputPassword.error_text = "Digite a senha" if not senha else""
-            tela_login.inputLogin.error_text="Digite o login" if not nome else""
+            # Se o nome ou a senha estiverem errados, mostramos os erros de forma apropriada
+            if nome != "carlos":
+                tela_login.inputLogin.error_text = "Login incorreto"
+            else:
+                tela_login.inputLogin.error_text = ""
+
+            if senha != "123":
+                tela_login.inputPassword.error_text = "Senha incorreta"
+            else:
+                tela_login.inputPassword.error_text = ""
+
+        # Atualiza a tela de login
         tela_login.update()
 
     tela_login = Login()
-    tela_login.btnEnter.on_click= changepagemenu
+    tela_login.btnEnter.on_click = changepagemenu
     tela_menu = Menu()
+
     def changeroutes(route):
-        # append adiciona um objeto view(tela)
+        # Adiciona a tela de login na rota principal
         page.views.append(
-            View(route = "/", controls=[tela_login])
-
+            View(route="/", controls=[tela_login])
         )
-        #perguntar se estou em outra rota
 
+        # Quando a rota for '/menu', adiciona a tela do menu
         if page.route == "/menu":
             page.views.append(
                 View(route="/menu", controls=[tela_menu])
-
             )
         page.update()
-
 
     page.on_route_change = changeroutes
     page.go(page.route)
